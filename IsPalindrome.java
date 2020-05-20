0cebe614503fd2f5e078639aa8ba2058c95140e3
mdiaz26 <mike.diaz006@gmail.com>
2020-05-20 14:15:59 -0400

    algo ignores non-letters

--- IsPalindrome.java
+++ IsPalindrome.java
@@ -4,17 +4,30 @@
 public class IsPalindrome{
 
     public static void main(String[] args){
-        for (String str : args) {
-            String word = str;
+        String letters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        for (String word : args) {
             int stringLength = word.length();
-            for (int i = 0; i < stringLength; i++){
-                int mirrorIndex = stringLength - (i +1);
-                if (word.charAt(i) != word.charAt(mirrorIndex)) {
+            int i = 0;
+            int mirrorIndex = stringLength - (i + 1);
+            while (i < mirrorIndex) {
+                while (letters.indexOf(word.charAt(i)) < 0 && i <= mirrorIndex) {
+                    i++;
+                }
+                while (letters.indexOf(word.charAt(mirrorIndex)) < 0 && mirrorIndex >= i) {
+                    System.out.println(letters.indexOf(word.charAt(mirrorIndex)));
+                    mirrorIndex--;
+                }
+                char leftLetter = word.charAt(i);
+                char rightLetter = word.charAt(mirrorIndex);
+                System.out.println(leftLetter + " - " + rightLetter);
+                if (leftLetter != rightLetter) {
                     System.out.println("false");
                     break;
                 }
+                i++;
+                mirrorIndex--;
             }
         }
-        System.out.println("truthy");
+        System.out.println("true");
     }
 }
